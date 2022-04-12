b958c8d29e26a23f9ba372f4304c19bcaf4893be
tallner <ctallner@hotmail.com>
2022-04-12 11:08:38 +0200

    ny kommit

--- src/main.js
+++ src/main.js
@@ -12,21 +12,55 @@ const birdUrl = new URL('./img/bird.jpg', import.meta.url);
 const dogUrl = new URL('./img/dog.jpg', import.meta.url);
 //console.log(dogImg);
 
+const imgURLs = [new URL('./img/bird.jpg', import.meta.url),
+                new URL('./img/dog.jpg', import.meta.url)];
+
 //birdImg.src = birdUrl.href;
 //dogImg.src = dogUrl.href;
 changeImg.src = birdUrl.href;
 
 let bytaBild = false;
+let currentSource = "";
+let i = 0;
+
 
 function changePic() {
-    if (bytaBild){
+    /* if (bytaBild){
         changeImg.src = birdUrl.href;
 
-    }else changeImg.src = dogUrl.href;
+    }else changeImg.src = dogUrl.href; */
+
+   /*  if (changeImg.src === birdUrl.href){
+        changeImg.src = dogUrl.href;
+
+    }else changeImg.src = birdUrl.href; */
+
+   /*  if (currentSource.localeCompare(birdUrl.href) === 0){
+        currentSource = dogUrl.href;
+
+    }else currentSource = birdUrl.href;
+
+    changeImg.src = currentSource; */
+
+   /*  if (changeImg.src.localeCompare(birdUrl.href) === 0){
+        changeImg.src = dogUrl.href;
+
+    }else changeImg.src = birdUrl.href; */
+
+    //changeImg.src = currentSource;
+
+    changeImg.src = imgURLs[i];
+    i++;
+    if (i>=imgURLs.length) i=0;
+
+
+
+  
     
-    bytaBild = !bytaBild;
+    //bytaBild = !bytaBild;
+
+    console.log(changeImg.src)
+    console.log(birdUrl.href)
+    console.log(dogUrl.href)
 
   }
-
-  console.log(changeImg.src)
-  console.log(dogUrl.href)
