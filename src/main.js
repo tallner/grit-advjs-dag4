8d0cf3da36120e5e53a29a9781b21babf1b6e959
tallner <ctallner@hotmail.com>
2022-04-12 10:35:07 +0200

    fler bilder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
-const img = document.querySelector('img');
+const birdImg = document.querySelector('#bird');
+const dogImg = document.querySelector('#dog');
 
-console.log(import.meta );
-const manUrl = new URL('./img/man.jpg', import.meta.url);
-console.log(manUrl);
+const changeImg = document.querySelector('#change');
+changeImg.addEventListener("click", changePic);
 
-img.src = manUrl.href;
+//console.log(import.meta);
+
+const birdUrl = new URL('./img/bird.jpg', import.meta.url);
+//console.log(birdUrl);
+
+const dogUrl = new URL('./img/dog.jpg', import.meta.url);
+//console.log(dogImg);
+
+//birdImg.src = birdUrl.href;
+//dogImg.src = dogUrl.href;
+changeImg.src = birdUrl.href;
+
+let bytaBild = false;
+
+function changePic() {
+    if (bytaBild){
+        changeImg.src = birdUrl.href;
+
+    }else changeImg.src = dogUrl.href;
+    
+    bytaBild = !bytaBild;
+
+  }
+
+  console.log(changeImg.src)
+  console.log(dogUrl.href)
